ac121f0c3e4e12d0ca0b90a16e64c088d4947c22
Long Yao <yl1984108@gmail.com>
2023-11-12 18:14:39 -0500

    add lock file

--- amtb_search/index.py
+++ amtb_search/index.py
@@ -1,4 +1,5 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-documents = SimpleDirectoryReader(input_dir="../amtb/doc/認識佛教", recursive=True).load_data()
+documents = SimpleDirectoryReader(input_dir="../../amtb/doc/認識佛教", recursive=True).load_data()
 index = VectorStoreIndex.from_documents(documents)
+index.storage_context.persist()
